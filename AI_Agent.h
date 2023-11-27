8c0462bc9b3365cbe82e5d38d6610d162072ca40
Ibrahim-Mallouka <panda.bornalex@gmail.com>
2023-11-27 18:06:38 +0100

    Adding AI_Agent

---
+++ AI_Agent.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+class AI_Agent
+{
+
+public:
+	void InitializedAgent(int particleID, float radius, sf::Vector2f spawnPosition);
+	void Update(sf::RenderWindow& window, float& deltaTime);
+
+
+	bool IsSuperPosed(sf::CircleShape& shape1, sf::CircleShape& shape2);
+	void SetTarget(sf::CircleShape& target);
+	void Chase(float deltaTime);
+
+	sf::Vector2f NormalizedVector(sf::Vector2f source);
+	sf::CircleShape& GetCircle();
+	sf::Vector2f& GetDirection();
+
+
+private:
+	int _id;
+	sf::CircleShape _circle;
+	sf::Vector2f _direction;
+	sf::CircleShape& _target;
+
+	float _speed;
+	float _mass;
+
+};
+
