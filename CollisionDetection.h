56f83266faeddfba8d6ccdd8aa4fea047d073846
Corentinrmt <corentin.remot@gmail.com>
2023-11-29 21:12:55 +0100

    Re organize collsision point h

--- CollisionDetection.h
+++ CollisionDetection.h
@@ -14,25 +14,33 @@ public :
 		rectList = rectListTemp;
 	}
 
-	/*bool PointInsideSquare(float, float, sf::RectangleShape)*/;
+	//bool PointInsideSquare(float, float, sf::RectangleShape);
 
-	bool CircleIsPartiallyInCircle(sf::CircleShape, sf::CircleShape, sf::Vector2f);
+	//bool CircleIsPartiallyInCircle(sf::CircleShape, sf::CircleShape, sf::Vector2f);
 
-	bool CircleIsInSquare(sf::CircleShape, sf::RectangleShape, sf::Vector2f);
+	//bool CircleIsInSquare(sf::CircleShape, sf::RectangleShape, sf::Vector2f);
 
-	bool CirclePartialyInSquare(sf::CircleShape, sf::RectangleShape, sf::Vector2f);
+	//bool CirclePartialyInSquare(sf::CircleShape, sf::RectangleShape, sf::Vector2f);
 
 	sf::Vector2f ClampCircleOutsideRectangles(sf::CircleShape&, std::list<sf::RectangleShape>, sf::Vector2f, sf::Vector2f);
 
 	sf::Vector2f ClampCircleInsideRectangle(sf::CircleShape&, sf::RectangleShape, sf::Vector2f, sf::Vector2f);
 
 
-	bool BulletTouchWall(Projectile&);
+	//bool BulletTouchWall(Projectile&);
 
 	void BulletsCollideWall(std::list<Projectile*>&);
 
 
 	std::list<sf::RectangleShape> rectList;
 
+private:
+	bool CircleIsPartiallyInCircle(sf::CircleShape, sf::CircleShape, sf::Vector2f);
+
+	bool CircleIsInSquare(sf::CircleShape, sf::RectangleShape, sf::Vector2f);
+
+	bool CirclePartialyInSquare(sf::CircleShape, sf::RectangleShape, sf::Vector2f);
+
+	bool BulletTouchWall(Projectile&);
 };
 
