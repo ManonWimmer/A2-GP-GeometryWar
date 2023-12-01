40f67a128e5dd7a8264f662f0738051f2521214c
Manon Wimmer <manonwimmer30@gmail.com>
2023-12-01 16:42:40 +0100

    game manager with CheckGameEnd and SpawnEnemy

--- GameManager.cpp
+++ GameManager.cpp
@@ -1,1 +1,62 @@
 #include "GameManager.h"
+#include "AI_Agent.h"
+#include "CollisionDetection.h"
+#include "ManagerEntity.h"
+
+
+GameManager::GameManager(ManagerEntity& managerEntity, CollisionDetection& collisionDetection, Player& player) 
+{
+	this->_managerEntity = managerEntity;
+	this->_collisionDetection = collisionDetection;
+	this->_player = player;
+}
+
+void GameManager::SpawnEnemy(float radius, sf::Vector2f spawnPosition, float speed)
+{
+	AI_Agent* ptrEnemy = new AI_Agent(_managerEntity, _collisionDetection, radius, spawnPosition, speed, _player);
+	_managerEntity.AddEntity(ptrEnemy);
+	_enemies.push_back(ptrEnemy); 
+}
+
+void GameManager::CheckEnemiesLife()
+{
+	if (_enemies.size() > 0)
+	{
+		std::list<AI_Agent*>::iterator it = _enemies.end();
+		_allEnemiesDead = true;
+
+		while (it != _enemies.end())
+		{
+			if (!((*it)->isDead))
+			{
+				_allEnemiesDead = false;
+			}
+			it++;
+		}
+	}
+}
+
+void GameManager::CheckPlayerLife() 
+{
+	_playerDead = (_player.pv <= 0) ? true : false;
+}
+
+void GameManager::CheckGameEnd()
+{
+	CheckEnemiesLife();
+	CheckPlayerLife();
+
+	if (_playerDead) 
+	{
+		std::cout << "Player dead, lose" << std::endl;
+		_gameLose = true;
+	}
+	else if (_allEnemiesDead) 
+	{
+		std::cout << "All enemies dead, won" << std::endl;
+		_gameWon = true;
+	}
+}
+
+
+
--- GameManager.h
+++ GameManager.h
@@ -1,5 +1,28 @@
 #pragma once
+#include <SFML/Graphics.hpp>
+#include "ManagerEntity.h"
+#include "AI_Agent.h"
+#include <list>
+
+class ManagerEntity;
+
 class GameManager
 {
+public :
+	GameManager(ManagerEntity&, CollisionDetection&, Player&);
+	void SpawnEnemy(float, sf::Vector2f, float);
+	void CheckEnemiesLife();
+	void CheckPlayerLife();
+	void CheckGameEnd(); // A changer plus tard par CheckLevelEnd ?
+
+private : 
+	ManagerEntity _managerEntity;
+	CollisionDetection _collisionDetection;
+	Player _player;
+	std::list<AI_Agent*> _enemies;
+	bool _allEnemiesDead;
+	bool _playerDead;
+	bool _gameWon;
+	bool _gameLose;
 };
 
