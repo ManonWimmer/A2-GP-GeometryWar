c2a923b47482f55bbedee8b157cbfd326cc6a2a0
Manon Wimmer <manonwimmer30@gmail.com>
2023-12-06 17:29:14 +0100

    camera coms + check if player

--- Camera.cpp
+++ Camera.cpp
@@ -10,12 +10,15 @@ void Camera::Update(sf::RenderWindow& window, float deltaTime)
 {
 	_shake = false;
 
-	if (_shake) window.setPosition(sf::Vector2i(100 + rand() % 25, 100 + rand() % 25));
+	//if (_shake) window.setPosition(sf::Vector2i(100 + rand() % 25, 100 + rand() % 25));
 
-	std::cout << "player pos " << _player->circleShape.getPosition().x << " " << _player->circleShape.getPosition().y << std::endl;
-	sf::Vector2i _playerPos = sf::Vector2i(_player->circleShape.getPosition().x, _player->circleShape.getPosition().y);
+	if (_player != nullptr) {
+		//std::cout << "player pos " << _player->circleShape.getPosition().x << " " << _player->circleShape.getPosition().y << std::endl;
+		sf::Vector2i _playerPos = sf::Vector2i(_player->circleShape.getPosition().x, _player->circleShape.getPosition().y);
 
-	window.setPosition(_playerPos);
-	_playerView.setCenter(_player->circleShape.getPosition());
-	window.setView(_playerView);
+		window.setPosition(_playerPos);
+		_playerView.setCenter(_player->circleShape.getPosition());
+		window.setView(_playerView);
+	}
+	
 }
