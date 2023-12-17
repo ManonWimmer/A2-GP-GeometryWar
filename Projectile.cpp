7c7b272d065873e8e0e67d720d6191af564801c2
Corentinrmt <corentin.remot@gmail.com>
2023-12-17 18:49:04 +0100

    projectile clean

--- Projectile.cpp
+++ Projectile.cpp
@@ -2,13 +2,9 @@
 #include <SFML/Graphics.hpp>
 
 Projectile::Projectile(ManagerEntity& managerEntity, CollisionDetection& collisionDetection, EntityType entityType, Faction entityFaction, CollisionType collisionType, sf::Vector2f origin, sf::Vector2f direction, float currentTime, int speed, sf::CircleShape projectileShape)
-	: Entity(managerEntity, collisionDetection, entityType, entityFaction, collisionType)
+	: Entity(managerEntity, collisionDetection, entityType, entityFaction, collisionType), _origin(origin), _direction(direction), CurrentTime(currentTime), _speed(speed), ProjectileShape(projectileShape)
 {
-	this->_origin = origin;
-	this->_direction = direction;
-	this->CurrentTime = currentTime;
-	this->_speed = speed;
-	this->ProjectileShape = projectileShape;
+	
 }
 
 void Projectile::Update(sf::RenderWindow& window, float deltaTime)
@@ -25,8 +21,6 @@ sf::CircleShape& Projectile::GetEntityCircleShape()
 void Projectile::MoveProjectile(float deltaTime)
 {
 	sf::Vector2f movement = _direction * static_cast<float>(_speed) * deltaTime;
-
-
 
 	ProjectileShape.move(movement);
 }
